c9469005d93c3b92f2b095a9afb528066b02f66d
Yiting Ren <yitingjen@gmail.com>
2022-11-15 16:34:45 +0800

    working attraction apis

---
+++ StoreData2Database.py
@@ -0,0 +1,141 @@
+import json
+import os
+import mysql.connector
+from mysql.connector import errorcode
+
+
+
+
+def image_url(urlstr):
+    imgurl=urlstr.split("https://")# split the string in the file
+    imgurl.remove("")# remove empty string in the list
+    imgurl=["https://" +item for item in imgurl]
+    # print(imgurl)
+    return imgurl
+
+
+abs_path=os.path.dirname(os.path.abspath(__file__))
+relative_path="data/taipei-attractions.json"
+full_path=os.path.join(abs_path, relative_path)
+f=open(full_path, 'r')
+
+data=json.load(f)
+f.close()
+data=data["result"]["results"]
+# cols=data[0].keys()
+# print(cols)
+# print(type(cols))
+# print(len(cols))
+# vals=data.values()
+# sql="insert into %s(%s) values(%s)"
+
+# Define tables
+TABLES={}
+TABLES['attractions'] = (
+    "CREATE TABLE `attractions` ("
+    "  `rate` int not null,"
+    "`transport` varchar(1000) not null,"
+    "`name` varchar(255) not null,"
+    "`date` varchar(20) not null,"
+    "`lng` float not null,"
+    "`REF_WP` int not null,"
+    "`avBegin` varchar(20) not null,"
+    "`langinfo` int not null,"
+    "`mrt` varchar(40) default 'NONE',"
+    "`SERIAL_NO` bigint not null,"
+    "`RowNumber` int not null,"
+    "`category` varchar(255) not null,"
+    "`MEMO_TIME` varchar(2000) default 'NONE',"
+    "`POI` varchar(4) not null,"
+    "`idpt` varchar(255) not null,"
+    "`lat` float not null,"
+    "`description` varchar(2000) not null,"
+    " `id` int(100) NOT NULL,"
+    "`avEnd` varchar(20) not null,"
+    "`address` varchar(100) not null,"
+    "  PRIMARY KEY (`id`) "
+    ") ENGINE=InnoDB")
+
+TABLES['imgURL'] = (
+    "CREATE TABLE `imgURL` ("
+    " `url_id` int not null auto_increment,"
+    "  `id` int(100) not null, "
+    "  `images` varchar(2000) NOT NULL,"
+    "  PRIMARY KEY (`url_id`),"
+   "  CONSTRAINT `imgURL_ibfk_1` FOREIGN KEY (`id`) REFERENCES `attractions` (`id`) ON DELETE CASCADE "
+    ") ENGINE=InnoDB")
+
+
+# Connect to database
+DB_NAME="TaipeiDayTrip"
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="0000",
+)
+
+cursor=mydb.cursor()
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+try:
+    cursor.execute("USE {}".format(DB_NAME))
+except mysql.connector.Error as err:
+    print("Database {} does not exists.".format(DB_NAME))
+    if err.errno == errorcode.ER_BAD_DB_ERROR:
+        create_database(cursor)
+        print("Database {} created successfully.".format(DB_NAME))
+        mydb.database = DB_NAME
+    else:
+        print(err)
+        exit(1)
+
+# Create tables
+for table_name in TABLES:
+    table_description = TABLES[table_name]
+    try:
+        print("Creating table {}: ".format(table_name), end='')
+        cursor.execute(table_description)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+
+cursor.close()
+
+# Insert Data
+add_attractions=("INSERT INTO attractions "
+              "(rate, transport, name, date, lng, REF_WP, avBegin, langinfo, mrt, SERIAL_NO, RowNumber, category, MEMO_TIME, POI, idpt, lat, description, id, avEnd, address) "
+              "VALUES (%(rate)s, %(direction)s, %(name)s, %(date)s, %(longitude)s, %(REF_WP)s, %(avBegin)s, %(langinfo)s, %(MRT)s, %(SERIAL_NO)s, %(RowNumber)s, %(CAT)s, %(MEMO_TIME)s, %(POI)s, %(idpt)s, %(latitude)s, %(description)s, %(_id)s, %(avEnd)s, %(address)s)")
+
+add_imgURL=("INSERT INTO imgURL"
+              "(id, images) "
+              "VALUES (%(_id)s, %(file)s)")
+
+cursor = mydb.cursor()
+
+
+for item in data:
+    # print(item.keys())
+    # print(item["_id"])
+    cursor.execute(add_attractions, item)
+    mydb.commit()
+    imgurl=image_url(item["file"])
+    for url in imgurl:
+        cursor.execute(add_imgURL, {"_id": item["_id"], "file":url})
+        mydb.commit()
+        
+
+cursor.close()
+mydb.close()
+
+
--- app.py
+++ app.py
@@ -1,7 +1,53 @@
+import mysql.connector
 from flask import *
+
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+
+
+def checkData(sql, val=()):
+	# return data type is dictionary
+	mydb=mysql.connector.connect(pool_name="mypool")
+	mycursor=mydb.cursor(dictionary=True)
+	mycursor.execute(sql, val)
+	myresult=mycursor.fetchall()
+	mycursor.close()
+	mydb.close()
+	return myresult
+
+
+def split_chunks(list, itemNo_chunk):
+	# How many elements each
+	# list should have itemNo_chunk
+	
+	# using list comprehension
+	result = [list[i * itemNo_chunk:(i + 1) * itemNo_chunk] for i in range((len(list) + itemNo_chunk - 1) // itemNo_chunk )]
+	final={}
+	for n in range(len(result)):
+		if n == len(result)-1:
+			final[n]=dict(Data=result[n], nextPage=None)
+		else:
+			final[n]=dict(Data=result[n], nextPage=n+1)
+	
+	# select_img="select images from imgURL where id =%s"
+	# val=(result[n])
+
+	return final
+
+def list_dict2value_list(lsts, key):
+  return [x.get(key) for x in lsts]
+
+def urls(list):
+	# put image urls into attraction list
+	for i in range(len(list)):
+		sql="select images from imgURL where id= %s"
+		val=(list[i]["id"],)
+		imgs=checkData(sql, val)
+		imgs=list_dict2value_list(imgs, "images")
+		list[i].update({"images":imgs})
+	
+	return list
 
 # Pages
 @app.route("/")
@@ -17,4 +63,99 @@ def booking():
 def thankyou():
 	return render_template("thankyou.html")
 
-app.run(port=3000)
+@app.route("/api/attractions", methods=['GET'])
+def api_attraction():
+	# Get parameters
+	page=request.args.get('page',0)
+	keyword=request.args.get('keyword')
+	page=int(page)
+	
+	# number of items in one page
+	NumInOnePage=12
+
+	print(keyword)
+	# Check if there is keyword?
+	if keyword: # There is keyword
+		print("there is keyword")
+		ambig_keyword='%'+keyword+'%'
+		print(ambig_keyword)
+		sql="select id, name, category, description, address, transport, mrt, lat, lng from attractions "\
+			"where (category= %s or description like %s)"
+		val=(keyword, ambig_keyword,)
+		attractions=checkData(sql, val)
+		if not attractions: return jsonify({"error": True, "message": "Empty search, change keyword or do not use keyword."}), 500
+
+		# put image urls into attraction list
+		attractions= urls(attractions)
+
+		# split attractions every 12 items
+		attractions=split_chunks(attractions, NumInOnePage)
+	else: # There is no keyword
+		print("there is no keyword")
+		sql="select id, name, category, description, address, transport, mrt, lat, lng from attractions "
+		attractions=checkData(sql)
+		if not attractions: return jsonify({"error": True, "message": "Something wrong on server side."}), 500
+		
+		# put image urls into attraction list
+		attractions= urls(attractions)
+
+
+		# split attractions every 12 items
+		attractions=split_chunks(attractions, NumInOnePage)
+
+
+	if page > len(attractions)-1:
+		return jsonify({"error": True, "message": "Max page is "+str(len(attractions)-1)}), 500
+	else:
+		return jsonify(attractions[page]), 200
+
+@app.route("/api/attraction/<attractionId>")
+def api_attractionId(attractionId):
+	attractionId=int(attractionId)
+
+	sql="select id, name, category, description, address, transport, mrt, lat, lng from attractions where id = %s"
+	val=(attractionId,)
+
+	try:
+		attractions=checkData(sql, val)
+		if not attractions: return jsonify({"error": True, "message": "ID does not exist."}), 400
+	except:
+		return jsonify({"error":True, "message":"Server internal error."}), 500
+		
+	# put image urls into attraction list
+	attractions= urls(attractions)
+
+	return jsonify(attractions), 200
+
+@app.route("/api/categories", methods=["GET"])
+def api_categories():
+	
+	sql="select category from attractions"
+	
+	try:
+		attractions=checkData(sql)
+	except:
+		return jsonify({"error":True, "message":"Server internal error."}), 500
+	
+	list = list_dict2value_list(attractions, "category")
+	print(list)
+
+	return jsonify({"data":list}), 200
+
+
+# MySQL Database config
+dbconfig={
+	"host":"localhost",
+	"user":"root",
+	"password":"0000",
+	"database":"TaipeiDayTrip"
+}
+
+# Create MySQL pooling
+mydb=mysql.connector.connect(
+	pool_name="mypool",
+	pool_size=20,
+	**dbconfig
+)
+mydb.close()
+app.run(port=3000, debug=True)
