c79ba8abfe5674d0429cd775c7ccd12d759e322b
Yiting Ren <yitingjen@gmail.com>
2022-11-17 09:49:55 +0800

    no repeated category, filter category & name keyword, use 'limit' in mysql, app.config modified

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import *
 app=Flask(__name__)
 app.config["JSON_AS_ASCII"]=False
 app.config["TEMPLATES_AUTO_RELOAD"]=True
+app.config['JSONIFY_MIMETYPE'] ="application/json;charset=utf-8"
+app.config["JSONIFY_PRETTYPRINT_REGULAR"]="True"
 
 
 def checkData(sql, val=()):
@@ -15,25 +17,6 @@ def checkData(sql, val=()):
 	mycursor.close()
 	mydb.close()
 	return myresult
-
-
-def split_chunks(list, itemNo_chunk):
-	# How many elements each
-	# list should have itemNo_chunk
-	
-	# using list comprehension
-	result = [list[i * itemNo_chunk:(i + 1) * itemNo_chunk] for i in range((len(list) + itemNo_chunk - 1) // itemNo_chunk )]
-	final={}
-	for n in range(len(result)):
-		if n == len(result)-1:
-			final[n]=dict(data=result[n], nextPage=None)
-		else:
-			final[n]=dict(data=result[n], nextPage=n+1)
-	
-	# select_img="select images from imgURL where id =%s"
-	# val=(result[n])
-
-	return final
 
 def list_dict2value_list(lsts, key):
   return [x.get(key) for x in lsts]
@@ -74,41 +57,37 @@ def api_attraction():
 	# number of items in one page
 	NumInOnePage=12
 
-	print(keyword)
 	# Check if there is keyword?
 	if keyword: # There is keyword
-		print("there is keyword")
 		ambig_keyword='%'+keyword+'%'
-		print(ambig_keyword)
 		sql="select id, name, category, description, address, transport, mrt, lat, lng from attractions "\
-			"where (category= %s or description like %s)"
-		val=(keyword, ambig_keyword,)
-		attractions=checkData(sql, val)
-		if not attractions: return jsonify({"error": True, "message": "Empty search, change keyword or do not use keyword."}), 500
-
-		# put image urls into attraction list
-		attractions= urls(attractions)
-
-		# split attractions every 12 items
-		attractions=split_chunks(attractions, NumInOnePage)
+			"where (category= %s or name like %s) "\
+			"order by id asc "\
+			"limit %s, %s"
+		val=(keyword, ambig_keyword,page*NumInOnePage, NumInOnePage,)
 	else: # There is no keyword
-		print("there is no keyword")
-		sql="select id, name, category, description, address, transport, mrt, lat, lng from attractions "
-		attractions=checkData(sql)
-		if not attractions: return jsonify({"error": True, "message": "Something wrong on server side."}), 500
+		sql="select id, name, category, description, address, transport, mrt, lat, lng from attractions order by id asc limit %s, %s "
+		val=(page*NumInOnePage, NumInOnePage,)
+	
+	# Search in mysql
+	try:
+		attractions=checkData(sql, val)
+		if  len(attractions) ==0: # if serch result is empty
+			return jsonify({"error": True, "message": "Empty result."}), 500
+	except mysql.connector.Error as err:
+		print(err) #print mysql error
+		return jsonify({"error": True, "message": "Server internal error."}), 500
 		
-		# put image urls into attraction list
-		attractions= urls(attractions)
+	# put image urls into attraction list
+	attractions= urls(attractions)
 
-
-		# split attractions every 12 items
-		attractions=split_chunks(attractions, NumInOnePage)
-
-
-	if page > len(attractions)-1:
-		return jsonify({"error": True, "message": "Max page is "+str(len(attractions)-1)}), 500
+	# Return results
+	if len(attractions) == NumInOnePage:
+		return jsonify(dict(data=attractions, nextPage=page+1)), 200
+		
 	else:
-		return jsonify(attractions[page]), 200
+		return jsonify(dict(data=attractions, nextPage=None)),200
+
 
 @app.route("/api/attraction/<attractionId>")
 def api_attractionId(attractionId):
@@ -119,8 +98,9 @@ def api_attractionId(attractionId):
 
 	try:
 		attractions=checkData(sql, val)
-		if not attractions: return jsonify({"error": True, "message": "ID does not exist."}), 400
-	except:
+		if len(attractions) ==0: return jsonify({"error": True, "message": "ID does not exist."}), 400 # if serch result is empty
+	except mysql.connector.Error as err:
+		print(err) #print mysql error
 		return jsonify({"error":True, "message":"Server internal error."}), 500
 		
 	# put image urls into attraction list
@@ -132,15 +112,15 @@ def api_attractionId(attractionId):
 @app.route("/api/categories", methods=["GET"])
 def api_categories():
 	
-	sql="select category from attractions"
+	sql="select distinct category from attractions"
 	
 	try:
 		attractions=checkData(sql)
-	except:
-		return jsonify({"error":True, "message":"Server internal error."}), 500
+	except mysql.connector.Error as err:
+		print(err) #print mysql error
+		return jsonify({"error":True, "message":"Server internal error."}), 500 
 	
 	list = list_dict2value_list(attractions, "category")
-	print(list)
 
 	return jsonify({"data":list}), 200
 
